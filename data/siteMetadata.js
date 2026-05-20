271e5fa421bdc643d7ddf59cc97542a82cb10d97
Samir Yuja <samiryuja@hotmail.com>
2026-05-19 23:26:53 -0400

    update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,29 +1,19 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Samir Yuja',
+  author: 'Samir Yuja',
+  headerTitle: 'Samir Yuja',
+  description: 'Backend & data engineer writing about building with AI',
   language: 'en-us',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  theme: 'system',
+  siteUrl: 'https://samiryuja.dev',
+  siteRepo: 'https://github.com/syuja/samiryuja.dev',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
-  // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
+  github: 'https://github.com/syuja',
+  linkedin: 'https://www.linkedin.com/in/samir-j-yuja/',
   locale: 'en-US',
-  // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
