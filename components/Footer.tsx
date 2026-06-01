70d8a38b70853931549180c17fc8f604083538e3
Samir Yuja <samiryuja@hotmail.com>
2026-06-01 08:39:43 -0400

    replace site title with motto in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,9 @@ export default function Footer() {
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
+          <Link href="/" className="italic">
+            Vamos con todo
+          </Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
